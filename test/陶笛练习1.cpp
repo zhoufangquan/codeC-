162efe92482303334dcfef63bafb53baed157015
zhoufangquan <2542154447@qq.com>
2021-12-12 10:28:25 +0800

    init

---
+++ test/陶笛练习1.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+
+    cout<<"| 1  -  -  -  | 1  -  -  -  | 1  -  -  -  | 1  -  -  -  ||";
+    cout<<"| 1  -  -  -  | 1  -  -  -  | 1  -  -  -  | 1  -  -  -  ||";
+    return 0;
+}
